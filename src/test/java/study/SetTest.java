103ea1d65d1f58cf81b38805c07d6a1d30efae4f
heoseungyeon <dia0312@naver.com>
2022-06-11 00:44:20 +0900

    test: SetTest 요구사항2 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,16 +2,26 @@ package study;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class SetTest {
 
   private Set<Integer> numbers;
+
+  private static Stream<Integer> argumentProvider(Set<Integer> numbers) {
+    return Stream.of(numbers.toArray(new Integer[0]));
+  }
+
+  // Test Case 구현
 
   @BeforeEach
   void setUp() {
@@ -22,11 +32,15 @@ public class SetTest {
     numbers.add(3);
   }
 
-  // Test Case 구현
-
   @Test
   void size() {
     int actual = numbers.size();
     assertThat(actual).isEqualTo(3);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3})
+  void contains_ShouldReturnTrueAllCase(int number) {
+    assertTrue(numbers.contains(number));
   }
 }
