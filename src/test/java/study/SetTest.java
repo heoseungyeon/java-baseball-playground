90172651c92ea6be2d779c32891e66c14e8a8fb4
heoseungyeon <dia0312@naver.com>
2022-06-11 00:52:53 +0900

    test: SetTest 요구사항3 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 
@@ -42,5 +43,11 @@ public class SetTest {
   @ValueSource(ints = {1, 2, 3})
   void contains_ShouldReturnTrueAllCase(int number) {
     assertTrue(numbers.contains(number));
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+  void contains_ShouldReturnTrueOrFalse(int element, boolean expected) {
+    assertThat(numbers.contains(element)).isEqualTo(expected);
   }
 }
