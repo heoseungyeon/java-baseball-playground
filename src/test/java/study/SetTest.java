4a2479f7df1bd266c39a94dd33ca0980aa4b957b
heoseungyeon <dia0312@naver.com>
2022-06-11 00:13:08 +0900

    test: SetTest 요구사항1 테스트 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,32 @@
+package study;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class SetTest {
+
+  private Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+  // Test Case 구현
+
+  @Test
+  void size() {
+    int actual = numbers.size();
+    assertThat(actual).isEqualTo(3);
+  }
+}
