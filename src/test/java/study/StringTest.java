acdfbe7340e72ba47dca50f2d57024c9fa800bed
heoseungyeon <dia0312@naver.com>
2022-06-10 23:56:31 +0900

    test: StringTest 요구사항1 테스트 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,27 @@
 package study;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+
 public class StringTest {
-    @Test
-    void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
-    }
+
+  @Test
+  void replace() {
+    String actual = "abc".replace("b", "d");
+    assertThat(actual).isEqualTo("adc");
+  }
+
+  @Test
+  void splitMoreThanTwoString() {
+    String[] actual = "1,2".split(",");
+    assertThat(actual).containsExactly("1", "2");
+  }
+
+  @Test
+  void splitOnlyOneString() {
+    String[] actual = "1".split(",");
+    assertThat(actual).containsOnly("1");
+  }
+
 }
